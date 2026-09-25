c96271644cd59724187edb98e82b05d993f24938
Vasiliy-Blokhin <vasiliyblo97@gmail.com>
2026-09-26 00:23:49 +0300

    fix

---
+++ params/settings.py
@@ -0,0 +1,2 @@
+project_url = 'https://github.com/Vasiliy-Blokhin/local_code_worker'
+ARCHIVE_URL = lambda project_url=project_url, branch='dev':  f'{project_url}/archive/refs/heads/{branch}.zip'
--- temp_extract/local_code_worker-main/temp_extract/local_code_worker-main/README.md
+++
@@ -1,1 +0,0 @@
-# local_code_worker
